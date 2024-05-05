eebd47691a8d5a3f2fe90db0afbcbf9d4181ee0e
Ritik Raj Pandey <ritikphenomenon@gmail.com>
2024-05-05 08:46:41 +0530

    first commit

---
+++ src/Kanban/Kanban.jsx
@@ -0,0 +1,12 @@
+
+import  Navbar  from '../nav/Navbar'
+import Board from '../components/Board'
+
+export default function Kanban() {
+  return (
+    <>
+    <Navbar />
+     <Board/>
+    </>
+  )
+}
